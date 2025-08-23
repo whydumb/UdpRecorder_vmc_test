0a5971e5fa603902ca3a61a286436c19d6129077
whydumb <108032052+whydumb@users.noreply.github.com>
2025-08-23 13:48:49 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
 group = "top.fifthlight.udprecorder"
 version = "1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.illposed.osc:javaosc-core:0.8")
+}
+
 application {
     mainClass = "UdpRecorder"
 }
